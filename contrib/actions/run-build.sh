6fa1822c25d61d0d72b6389b4b314e779d66c272
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-04-13 14:17:27 +0200

    Update files

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -5,9 +5,9 @@ set -e
 export BROKEN=1
 export GLUON_AUTOREMOVE=1
 export GLUON_DEPRECATED=1
-export GLUON_SITEDIR="contrib/ci/minimal-site"
+export GLUON_SITEDIR="site"
 export GLUON_TARGET="$1"
 export BUILD_LOG=1
 
 make update
-make -j2 V=s
+make -j$(nproc||printf "2") V=s
