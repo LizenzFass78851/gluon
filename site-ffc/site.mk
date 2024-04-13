4c3708c3e19c692c5d45eb7be8bb0bb3163f8c03
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-04-13 14:29:47 +0200

    Update site

---
+++ site-ffc/site.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Unlicense
+# SPDX-FileCopyrightText: 2023 Freifunk Chemnitz e.V. <info@chemnitz.freifunk.net>
+
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)-multi
+
+GLUON_AUTOUPDATER_BRANCH ?= stable
+GLUON_AUTOUPDATER_ENABLED ?= 1
+GLUON_PRIORITY ?= 14
+GLUON_LANGS ?= de
+GLUON_WLAN_MESH ?= 11s
+GLUON_REGION ?= eu
+GLUON_MULTIDOMAIN = 1
+GLUON_DEPRECATED ?= update
