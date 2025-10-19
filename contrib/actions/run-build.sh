d53684d299c3d8102274658bd82310aef5b490dc
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-10-19 14:30:59 +0200

    Update files

--- contrib/actions/run-build.sh
+++ contrib/actions/run-build.sh
@@ -5,9 +5,10 @@ set -e
 export BROKEN=1
 export GLUON_AUTOREMOVE=1
 export GLUON_DEPRECATED=1
-export GLUON_SITEDIR="contrib/ci/minimal-site"
+export GLUON_SITEDIR="site-ffc"
 export GLUON_TARGET="$1"
 export BUILD_LOG=1
+export DEFAULT_GLUON_RELEASE="b$(date '+%Y%m%d')"
 
 BUILD_THREADS="$(($(nproc) + 1))"
 
